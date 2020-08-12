b4f6041105aa799b4907cb8d07fd0b7c4a7d9ada
ozgeonec <ozgenuronec@gmail.com>
2020-08-12 22:02:30 +0300

    meme generator working

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -10,6 +10,7 @@ class MemeGenerator extends React.Component{
             randomImg: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: []
         }
+        this.handleChange=this.handleChange.bind(this)
     }
         componentDidMount(){
             fetch("https://api.imgflip.com/get_memes")
@@ -20,14 +21,25 @@ class MemeGenerator extends React.Component{
                     this.setState({allMemeImgs:memes})
                 })
         }
+        handleChange(event){
+            const {name, value} = event.target
+            this.setState({[name]:value})
+        }
 
     render(){
         return(
             <div>
                 <form className="meme-form">
+                    <input type="text" name="topText" placeholder="Top Text" value={this.state.topText} onChange={this.handleChange}/>
+                    <input type="text" name="buttomText" placeholder="Buttom Text"  value={this.state.buttomText} onChange={this.handleChange}/>
 
                     <button>Gen</button>
                 </form>
+                <div className="meme">
+                    <img src={this.state.randomImg} alt=""/>
+                    <h2 className="top"> {this.state.topText}</h2>
+                    <h2 className="buttom"> {this.state.buttomText}</h2>
+                </div>
 
             </div>
         )
